3f24adb6862fc7ed43292019b501cbcbffed52e9
vikas <vikas.jain8596@gmail.com>
2019-02-27 15:32:44 +0530

    added help,jokes,facts functionalities

--- Play.py
+++ Play.py
@@ -7,13 +7,15 @@ import webbrowser as wb
 import speech_recognition as sr
 import audio_find as find
 import training as tr
+import jokes as joke
+import facts
 import string
 check = 0
-input_format = 1
+input_mode = 1
 
-# input_format = 0 for offline mode
-# input_format = 1 for speech to text
-# input_format = 2 for text mode
+# input_mode = 0 for offline mode
+# input_mode = 1 for speech to text
+# input_mode = 2 for text mode in online mode
 
 
 def speak(audiostring):
@@ -26,7 +28,7 @@ def recordAudio():
     # user input ( speech to text )
     r = sr.Recognizer()
     global check
-    global input_format
+    global input_mode
     with sr.Microphone(device_index=1) as source:
         r.energy_threshold = 280
         r.dynamic_energy_ratio = 0.1
@@ -49,7 +51,7 @@ def recordAudio():
             speak("Say yes if ok or say anything else otherwise!")
             response = recordAudio()
             if response == 'Yes' or response == 'yes':
-                input_format = 2
+                input_mode = 2
         if check == 3:
             speak("Good Bye!")
             exit()
@@ -60,7 +62,7 @@ def recordAudio():
         speak("Press Y for yes and then enter. Press anything else otherwise!")
         response = input()
         if response == 'Y' or response == 'y':
-            input_format = 0
+            input_mode = 0
         else:
             exit()
 
@@ -74,36 +76,57 @@ def search_data(data):
             exit()
         elif "hi" in data or "hello" in data or "hey" in data or "hey Dexter" in data:
             speak("Hi, how can I help you ?")
+        elif "help" in data:
+            speak("Try one of the following:")
+            speak("Say: ")
+            speak("hello to get response,")
+            speak("open youtube, open whatsapp")
+            speak("search google")
+            speak("Calculate 2+2")
+            speak("how is the weather outside")
+            speak("introduce yourself")
+            speak("Tell me a joke")
+            speak("Tell me some interesting facts")
+        elif "how's the Josh" in data or "how is the Josh" in data:
+            speak("High Sir!")
         elif "time now" in data or "time please" in data or "tell me the time" in data:
             speak("now the time is "+ ctime())
         elif "your name" in data:
             speak("My name is Dexter 1.0")
+        elif "joke" in data or "Joke" in data:
+            speak("One of the interesting jokes I remember is ")
+            joke.telljoke()
+        elif "fact" in data or "Fact" in data:
+            speak("One of the interesting facts I remember is ")
+            facts.tellFact()
         elif "How old are you" in data or "your age" in data:
             speak("I don't remember the exact day I born, but I can say I'm way younger than you")
-        elif "about yourself" in data or "introduction" in data:
+        elif "about yourself" in data or "introduction" in data or "introduce yourself" in data:
             speak("Hello, My name is Dexter 1.0. I'm your personal assistant. I was created as a minor project. "
                   "My skills are: I can open YouTube for you, tell you the current time, open some of the apps "
                   "installed on this pc, do arithmetic calculations, get you updated on the current activities "
                   "running across the world.")
             speak("And most importantly, I give my best when it comes to entertain you.")
             speak("My best part is, 'I never stop learning!'")
-
+        elif "weather" in data:
+            speak("let's see what google says about current weather")
+            wb.open("http://www.google.com/search?client=firefox-b-d&q=" + data)
         # open commands...
 
         elif "open YouTube" in data:
-            if input_format:
+            if input_mode:
                 speak("opening YouTube")
                 wb.open("https://www.youtube.com")
             else:
                 speak("Sorry! This can not be executed in offline mode")
         elif "open Facebook" in data:
-            if input_format:
+            if input_mode:
                 speak("opening facebook")
                 wb.open("https://www.facebook.com")
             else:
                 speak("Sorry! This can not be executed in offline mode")
         elif "open WhatsApp" in data:
-            if input_format:
+            if input_mode:
                 speak("opening whatsapp")
                 wb.open("https://web.whatsapp.com/", new=0, autoraise=True)
             else:
@@ -112,12 +135,12 @@ def search_data(data):
             speak("opening mail")
             os.system("start outlookmail:")
         elif "YouTube" in data:
-            if input_format == 1:
+            if input_mode == 1:
                 speak("what to search")
                 query = recordAudio()
                 speak("opening youtube")
                 wb.open("https://www.youtube.com/results?search_query="+query, new = 0 , autoraise = True)
-            elif input_format == 2:
+            elif input_mode == 2:
                 speak("what to search")
                 query = input()
                 speak("opening youtube")
@@ -127,12 +150,12 @@ def search_data(data):
         elif "your significance" in data:
             speak("ghar say mai nikla tanha akela, saath me mere kaun hai, BOT hai mera")
         elif "Google" in data:
-            if input_format == 1:
+            if input_mode == 1:
                 speak(" what to search")
                 term = recordAudio()
                 wb.open("http://www.google.com/search?client=firefox-b-d&q="+term)
                 speak("Here is what I found on Google")
-            elif input_format == 2:
+            elif input_mode == 2:
                 speak("what to search")
                 term = input()
                 wb.open("http://www.google.com/search?client=firefox-b-d&q=" + term)
@@ -172,8 +195,8 @@ def search_data(data):
 speak("Welcome back")
 
 while 1:
-    if input_format == 1:
-        data = recordAudio()
+    if input_mode == 1:
+        data = input()
     else:
         data = input()
     search_data(data)
